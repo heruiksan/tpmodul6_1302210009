253edd0c7fc5e985de401887d10c8c3a723f757b
heruiksan <heruiksan43@gmail.com>
2023-03-26 22:17:03 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210009/Program.cs
+++ tpmodul6_1302210009/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_1302210009;
+
+SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - Heru Ikhsan Maulana");
+sayaTubeVideo.IncreasePlayCount(10000001);
+sayaTubeVideo.PrintVideoDetail();
---
+++ tpmodul6_1302210009/SayaTubeVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210009
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Debug.Assert(title.Length <= 100 && title != null, "judul video memiliki panjang maksimal 100 karakter dan tidak berupa null.");
+            this.title = title;
+            var random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            Debug.Assert(playCount > 10000000 || playCount<0, "Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");
+            try
+            {
+                this.playCount = checked(this.playCount + playCount);
+            }
+                catch (OverflowException e)
+            {
+                Console.WriteLine("Overflow exception : " + e.Message);
+            }
+        }
+
+        public void PrintVideoDetail()
+        {
+            Console.WriteLine("ID : " + id);
+            Console.WriteLine("Title : " + title);
+            Console.WriteLine("playCount : " + playCount);
+        }
+    }
+}
